1a0f0dbcc1254ff5b052b93f04825f9956d37354
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-04-02 17:27:59 -0300

    Models separeted from WebGL buffers. This buffers are now in the Graphic object

--- graphic.js
+++ graphic.js
@@ -17,9 +17,32 @@ export class Graphic {
 
 	/* private methods */
 
+	_initBuffers() {
+		
+		this.webgl_position_buffer = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.webgl_position_buffer);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER,
+				   new Float32Array(this.model.getPosition()),
+				   this.gl.STATIC_DRAW);
+	
+		this.webgl_color_buffer = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.webgl_color_buffer);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER,
+				   new Float32Array(this.model.getColor()),
+				   this.gl.STATIC_DRAW);
+
+		this.webgl_index_buffer = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
+		this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER,
+				   new Uint16Array(this.model.getIndexes()),
+				   this.gl.STATIC_DRAW);
+	}
+
 	_init() {
 		// initialize model matrix element
 		this.matrix = mat4.create();
+
+		this._initBuffers();
 	}
 
 	_updateTransformations(matrix) {
@@ -40,11 +63,44 @@ export class Graphic {
 		this.gl.uniformMatrix4fv(uniformMatrixModel, false, this.matrix);
 	}
 
+	_bindBuffers() {
+		// connect position data in local buffers 
+		// with shader vertex position buffer
+		var vertexPositionAttribute = this.program.findAttribute("aVertexPosition");
+		
+		this.gl.enableVertexAttribArray(vertexPositionAttribute);
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.webgl_position_buffer);
+		this.gl.vertexAttribPointer(vertexPositionAttribute, 3, this.gl.FLOAT, false, 0, 0);
+
+		// connect color data in local buffers
+		// with shader vertex color buffer
+		var vertexColorAttribute = this.program.findAttribute("aVertexColor");
+		
+		this.gl.enableVertexAttribArray(vertexColorAttribute);
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.webgl_color_buffer);
+		this.gl.vertexAttribPointer(vertexColorAttribute, 3, this.gl.FLOAT, false, 0, 0);
+
+		// connect indexes data in local buffers
+		// with GPU index buffer
+		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
+	}
+
 	_animate() {
+		
 		var i;
+		
 		for (i = 0; i < this.ts.length; i++) {
 			this.ts[i].update();
 		}
+	}
+
+	_draw() {
+	
+		this._bindBuffers();
+
+		this.gl.drawElements(this.gl.TRIANGLE_STRIP,
+				     this.model.getIndexes().length,
+				     this.gl.UNSIGNED_SHORT, 0);
 	}
 
 	/* public methods */
@@ -57,7 +113,7 @@ export class Graphic {
 
 		this._bindTransformations();
 		
-		this.model.draw(this.program);
+		this._draw();
 
 		this._animate();
 	}
--- grid.js
+++ grid.js
@@ -54,67 +54,28 @@ export class Grid {
 		}
 	}
 
-	_generateBuffers() {
+	_init() {
 		// generates a GRID defined by
 		// 'cols' and 'rows'
 		// with colors per vertex
 		// and indexes to render it
+		
 		this._createModel();
 		this._createIndexes();
 	}
 
-	_init() {
-		this._generateBuffers();
-
-		this.webgl_position_buffer = this.gl.createBuffer();
-		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.webgl_position_buffer);
-		this.gl.bufferData(this.gl.ARRAY_BUFFER,
-				   new Float32Array(this.position_buffer),
-				   this.gl.STATIC_DRAW);
-	
-		this.webgl_color_buffer = this.gl.createBuffer();
-		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.webgl_color_buffer);
-		this.gl.bufferData(this.gl.ARRAY_BUFFER,
-				   new Float32Array(this.color_buffer), 
-				   this.gl.STATIC_DRAW);
-
-		this.webgl_index_buffer = this.gl.createBuffer();
-		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
-		this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER,
-				   new Uint16Array(this.index_buffer),
-				   this.gl.STATIC_DRAW);
-	}
-
-	_bindBuffers(program) {
-		// connect position data in local buffers 
-		// with shader vertex position buffer
-		var vertexPositionAttribute = program.findAttribute("aVertexPosition");
-		
-		this.gl.enableVertexAttribArray(vertexPositionAttribute);
-		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.webgl_position_buffer);
-		this.gl.vertexAttribPointer(vertexPositionAttribute, 3, this.gl.FLOAT, false, 0, 0);
-
-		// connect color data in local buffers
-		// with shader vertex color buffer
-		var vertexColorAttribute = program.findAttribute("aVertexColor");
-		
-		this.gl.enableVertexAttribArray(vertexColorAttribute);
-		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.webgl_color_buffer);
-		this.gl.vertexAttribPointer(vertexColorAttribute, 3, this.gl.FLOAT, false, 0, 0);
-
-		// connect indexes data in local buffers
-		// with GPU index buffer
-		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
-	}
-
 	/* public methods */
 
-	draw(program) {
+	getPosition() {
+		return this.position_buffer;
+	}
 
-		this._bindBuffers(program);
+	getColor() {
+		return this.color_buffer;
+	}
 
-		this.gl.drawElements(this.gl.TRIANGLE_STRIP,
-				     this.index_buffer.length,
-				     this.gl.UNSIGNED_SHORT, 0);
+	getIndexes() {
+		return this.index_buffer;
 	}
 }
+
