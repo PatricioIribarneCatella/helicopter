06db2806f5315f81e3956bc0bac24c0da8063d4d
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-05-14 13:38:27 -0300

    Add more blank tests for the other curves

--- curve-test.js
+++ curve-test.js
@@ -4,7 +4,7 @@ import {Camera} from './scene/camera.js';
 
 import {ShaderProgram} from './shaders/program.js';
 
-import {BezierCuad} from './curves/bezier.js';
+import {BezierCuad, BezierCubic} from './curves/bezier.js';
 
 import {Rotation} from './transformations/rotation.js';
 
@@ -17,9 +17,11 @@ export class CurveApp extends App {
 		super(gl, canvas);
 	}
 
-	/* public methods */
+	/* private methods */
 
-	start() {
+	_bezierCuadTest() {
+		
+		console.log("Bezier Cuadratic Test\n");
 
 		var c = new BezierCuad([[0.0, 0.0, 0.0],[1.0, 1.0, 0.0],[2.0, 0.0, 0.0]]);
 
@@ -31,6 +33,42 @@ export class CurveApp extends App {
 
 			console.log(pos);
 		}
+	}
+
+	_bezierCubicTest() {
+		
+		console.log("Bezier Cubic Test\n");
+/*
+		var c = new BezierCubic([[0.0, 0.0, 0.0],[1.0, 1.0, 0.0],[2.0, 0.0, 0.0],[]]);
+
+		var N = 10, pos;
+
+		for (var i = 0; i < N; i++) {
+		
+			pos = c.get(i / N);
+
+			console.log(pos);
+		}
+*/
+	}
+
+	_bsplineCuadTest() {
+	
+		console.log("BSpline Cuadratic Test\n");
+	}
+
+	_bsplineCubicTest() {
+	
+		console.log("BSpline Cubic Test\n");
+	}
+
+	/* public methods */
+
+	start() {
+		this._bezierCuadTest();
+		this._bezierCubicTest();
+		this._bsplineCuadTest();
+		this._bsplineCubicTest();
 	}
 }
 
